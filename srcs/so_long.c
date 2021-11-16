3916f046c8456046dc091451b1824138e4ef0b82
Richard CORENTIN <rcorenti@e2r9p8.clusters.42paris.fr>
2021-11-16 01:32:02 +0100

    go

--- srcs/so_long.c
+++ srcs/so_long.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rcorenti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/14 05:04:13 by rcorenti          #+#    #+#             */
-/*   Updated: 2021/08/14 05:05:51 by rcorenti         ###   ########.fr       */
+/*   Created: 2021/11/16 00:14:48 by rcorenti          #+#    #+#             */
+/*   Updated: 2021/11/16 01:31:11 by rcorenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,20 @@
 #include <stdio.h>
 #include <errno.h>
 #include <unistd.h>
+#include "so_long.h"
 
 int	main(int argc, char **argv)
 {
-    if (argc != 2)
-    {
-        printf("Error\nNot enough or too many arguments\n");
-        return (0);
-    }
-    
-    return (0);
+	t_winit		winit;
+	t_imgsetup	imgsetup;
+
+	if (argc != 2)
+	{
+		printf("Error\nNot enough or too many arguments\n");
+		return (0);
+	}
+	winit.mlx = mlx_init();
+	winit.win = mlx_new_windows(winit.mlx, WIN_WIDTH, WIN_HEIGHT, "So Long !");
+	
+	return (0);
 }
